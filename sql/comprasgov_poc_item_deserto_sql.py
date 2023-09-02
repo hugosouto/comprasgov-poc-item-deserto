8e97e4126cadd8c3d5624b357c2d345f4e97bf27
Hugo Souto <hugomsouto@gmail.com>
2023-09-02 16:15:53 -0300

    Add query for testing data

--- sql/comprasgov_poc_item_deserto_sql.py
+++ sql/comprasgov_poc_item_deserto_sql.py
@@ -1,3 +1,15 @@
-sql_query = """
-SELECT * FROM analises.poc_item_deserto_consolidada LIMIT 100
+train_sql_query = """
+SELECT *
+FROM analises.poc_item_deserto_consolidada
+WHERE 1=1
+    AND valor_estimado IS NOT NULL 
+    AND data_hora_prevista_abertura_sp < '2023-08-10 00:00:00.000'
+"""
+
+test_sql_query = """
+SELECT *
+FROM analises.poc_item_deserto_consolidada
+WHERE 1=1
+    AND valor_estimado IS NOT NULL 
+    AND data_hora_prevista_abertura_sp > '2023-08-10 00:00:00.000'
 """
