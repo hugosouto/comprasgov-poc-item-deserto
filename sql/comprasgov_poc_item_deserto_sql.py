5a83251a6fdeaf0fd7d3de0703087ab34c97c7cb
Hugo Souto <hugomsouto@gmail.com>
2023-09-05 17:12:05 -0300

    Update file

--- sql/comprasgov_poc_item_deserto_sql.py
+++ sql/comprasgov_poc_item_deserto_sql.py
@@ -1,9 +1,16 @@
+full_sql_query = """
+SELECT *
+FROM analises.poc_item_deserto_consolidada
+WHERE 1=1
+    AND valor_estimado IS NOT NULL 
+"""
+
 train_sql_query = """
 SELECT *
 FROM analises.poc_item_deserto_consolidada
 WHERE 1=1
     AND valor_estimado IS NOT NULL 
-    AND data_hora_prevista_abertura_sp < '2023-08-10 00:00:00.000'
+    AND data_hora_prevista_abertura_sp < '2023-08-01 00:00:00.000'
 """
 
 test_sql_query = """
@@ -11,5 +18,5 @@ SELECT *
 FROM analises.poc_item_deserto_consolidada
 WHERE 1=1
     AND valor_estimado IS NOT NULL 
-    AND data_hora_prevista_abertura_sp > '2023-08-10 00:00:00.000'
+    AND data_hora_prevista_abertura_sp >= '2023-08-01 00:00:00.000'
 """
